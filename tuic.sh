dc61660cf8b76087a7b33b9158329d7f1e576077
ChengMoo <67187215+ChengMoo@users.noreply.github.com>
2022-09-13 20:13:15 +0800

    Update tuic.sh

--- tuic.sh
+++ tuic.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
 mkdir /opt/tuic && cd /opt/tuic
+rm -rf tuic
 wget https://github.com/EAimTY/tuic/releases/download/0.8.5/tuic-server-0.8.5-x86_64-linux-gnu
 chmod +x tuic-server-0.8.5-x86_64-linux-gnu
+mv tuic-server* tuic
 
 cat > tuic.json <<-EOF
 {
     "port": 8443,
     "token": ["999999999"],
-    "certificate": "/opt/tuic/fullchain.pem",
-    "private_key": "/opt/tuic/privkey.pem",
+    "certificate": "/usr/local/etc/xray/self_signed_cert.pem",
+    "private_key": "/usr/local/etc/xray/self_signed_key.pem",
     "ip": "::",
     "congestion_controller": "bbr",
     "alpn": ["h3"]
@@ -26,7 +28,7 @@ After=network.target
 [Service]
 User=root
 WorkingDirectory=/opt/tuic
-ExecStart=/opt/tuic/tuic-server-0.8.5-x86_64-linux-gnu -c config.json
+ExecStart=/opt/tuic/tuic -c config.json
 Restart=on-failure
 RestartPreventExitStatus=1
 RestartSec=5
@@ -35,12 +37,10 @@ RestartSec=5
 WantedBy=multi-user.target
 EOF
 
-apt install socat
-
-curl  https://get.acme.sh | sh -s email= 123456@qq.com
-
+#apt install socat
+#curl  https://get.acme.sh | sh -s email= 123456@qq.com
 #~/.acme.sh/acme.sh --install-cert -d yourdomain.com
 #--fullchain-file    /opt/tuic/fullchain.pem    \
 #--key-file       /opt/tuic/privkey.pem  \
 
-#systemctl enable --now tuic.service
+systemctl enable --now tuic.service
